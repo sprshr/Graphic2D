b3e4832d26ec11579ca41febf3d83cf786852e1e
Sepehr Sahraian <sep3831@gmail.com>
2022-09-25 12:57:36 -0700

    eyebrows, mouth & nose drawn

--- DrawingCanvas.java
+++ DrawingCanvas.java
@@ -34,5 +34,33 @@ public class DrawingCanvas extends JComponent{
 		g.fillOval(285, 200, 30, 30);
 		g.setColor(new Color(139, 82, 8));
 		g.fillOval(292, 214, 10, 10);
+		// right eyebrow
+		g.setStroke(new BasicStroke(3));
+		g.setColor(Color.black);
+		for (int Xvalue = 275; Xvalue <= 325; Xvalue += 5) {
+			g.drawArc(Xvalue, 185, 70, 20, 175, 25);
+		}
+		// left eye brow
+		for (int Xvalue = 145; Xvalue <= 200; Xvalue += 5) {
+			g.drawArc(Xvalue, 185, 70, 20, 175, 25);
+		}
+		// nose
+		g.setStroke(new BasicStroke(2));
+		g.drawArc(223, 280, 20, 20, 180, 185);
+		g.drawArc(243, 280, 20, 20, 180, 185);
+		g.drawLine(223, 290, 238, 250);
+		// mouth
+		//g.drawArc(180, 330, 20, 20, 180, 185);
+		//g.drawArc(200, 330, 20, 20, 5, 180);
+		int xValue = 175;
+		int startAngle = 180;
+		int arcAngle = 180;
+		while(xValue <= 275){
+			g.drawArc(xValue += 10, 340, 10, 10, startAngle, arcAngle);
+			startAngle = Math.abs(startAngle - 185);
+			arcAngle = Math.abs(arcAngle - 365);
+			
+		}
 	}
+
 }
